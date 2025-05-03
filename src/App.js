7016b37b8afc89c559b49e9636be341ba1c0bd94
Tanishka Verma <tanishkaverma43@gmail.com>
2025-05-03 17:29:56 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import men_banner from "./Components/Assets/banner_mens.png";
 import kid_banner from "./Components/Assets/banner_kids.png";
 import LoginSignup from "./Pages/LoginSignup";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = 'https://jewelleryshop-backend-y0v1.onrender.com';
 export const currency = '₹';
 
 function App() {
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -11,10 +11,10 @@ const Shop = () => {
   const [newcollection, setNewCollection] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/popularinwomen') 
+    fetch('https://jewelleryshop-backend-y0v1.onrender.com/popularinwomen') 
             .then((res) => res.json()) 
             .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
+    fetch('https://jewelleryshop-backend-y0v1.onrender.com/newcollections') 
             .then((res) => res.json()) 
             .then((data) => setNewCollection(data))
     }
